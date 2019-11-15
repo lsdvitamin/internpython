4f07f37de10d6853dccec254d0b1807d93705ced
lsdvitamin <lsdvitamin7@gmail.com>
2019-11-15 22:53:05 +0300

    add_lib_grph

---
+++ graph.py
@@ -0,0 +1,13 @@
+import math
+import graphics as gr
+
+
+def build_house(w, corn, width):
+    """ Функция, которая строит дом """
+    pass
+ 
+
+window = gr.GraphWin("Russian game", 500, 500)
+build_house(window, gr.Point(100, 100), 100)
+
+print("Ура! Дом построен")
